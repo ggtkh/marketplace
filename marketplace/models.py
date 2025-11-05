adf8315e1bdd4e66b75a1ce3bad1ed798a62bbb9
ggtkh <pashaazarov2008@gmail.com>
2025-11-05 23:30:39 +0200

    подготовка модели и функций вида, проверка работоспособности модели и доступа к ней через шаблон

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Good(models.Model):
+    title = models.CharField(max_length=255, default="Some Piece of Scrap")
+    description = models.CharField(max_length=500, default=title)
+    price = models.IntegerField()
+    posted_at = models.DateField(default=timezone.now)
+    main_picture = models.URLField()
+    hover_picture = models.URLField(default=main_picture)
+    pictures_list = models.JSONField(default=list)
+    is_available = models.BooleanField()
+    reviews_qty = models.IntegerField()
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Good
 
 # Create your views here.
+
+def index(request):
+    goods = Good.objects.all()
+    return render(request, 'testpage.html', {'goods': goods})
