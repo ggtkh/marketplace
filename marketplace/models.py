2cedd2faea8c9cf47a8afd7e0570d638b3862233
ggtkh <pashaazarov2008@gmail.com>
2025-11-06 12:36:26 +0200

    add some fields to the Good model

--- marketplace/models.py
+++ marketplace/models.py
@@ -6,11 +6,14 @@ from django.utils import timezone
 
 class Good(models.Model):
     title = models.CharField(max_length=255, default="Some Piece of Scrap")
-    description = models.CharField(max_length=500, default=title)
-    price = models.IntegerField()
+    description = models.CharField(max_length=500, default=title.name)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    brand = models.CharField(max_length=20, default=None)
     posted_at = models.DateField(default=timezone.now)
     main_picture = models.URLField()
     hover_picture = models.URLField(default=main_picture)
     pictures_list = models.JSONField(default=list)
     is_available = models.BooleanField()
     reviews_qty = models.IntegerField()
+    characteristics = models.JSONField(default=dict)
+    tags = models.JSONField(default=list)
