7e9761f7d16d8ed39da26a48ea8af52fe535db57
ggtkh <pashaazarov2008@gmail.com>
2026-06-24 20:12:31 +0000

    adaptive staticfiles path

--- base/settings.py
+++ base/settings.py
@@ -120,7 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/fgt/marketplace/staticfiles'
+# STATIC_ROOT = '/home/fgt/marketplace/staticfiles'
+
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
+
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+else:
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
