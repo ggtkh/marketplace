d5bff07bf9cb4515757415d6264830e1711b3eec
ggtkh <pashaazarov2008@gmail.com>
2026-06-24 13:39:33 +0000

    забыл

--- base/settings.py
+++ base/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)90vgg%dw)m0!wfo&a9-k!w&+(zfh0e53#8l-^zq_8s_p%c)(v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fgt.pythonanywhere.com']
 
 
 # Application definition
@@ -120,8 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/fgt/marketplace/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
